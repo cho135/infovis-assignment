50a14380b45bee9fc48fc78b1ea7294f056ce8c5
Clemens <e12323827@student.tuwien.ac.at>
2026-06-20 13:43:33 +0200

    updated butterfly chart

--- butterflyChart.js
+++ butterflyChart.js
@@ -1,19 +1,33 @@
 let butterflySvg, butterflyWidth, butterflyHeight;
-const centerOffset = 40
+const centerOffset = 50
 const butterflyMargin = { top: 50, right: 30, bottom: 30, left: 30};
 let butterFlyDefaultCountries = ["Aruba", "Niger", "Saint Lucia", "Yemen", "Bangladesh"];
 let butterFlyData = []
+let renderedData = []
 
 
 function initButterflyChart() {
-  setupDefaultData(2003);
+  //data setup
+  for (const country of healthInsuranceData.map(d => d["Entity"])) {
+    const dataForCountry = joinData(country, healthInsuranceData, "Share of population covered by health insurance (ILO (2014))")
+    for (const dataPoint of dataForCountry) {
+      butterFlyData.push({
+        Entity: country,
+        Year: dataPoint.year,
+        Insurance: dataPoint.value,
+        Mortality: dataPoint.mortality
+      })
+    }
+  }
+
+  renderedData = butterFlyData.filter(d => d.Year == 2009)
 
   const container = document.getElementById("butterfly-chart-svg");
   //ToDo: Fix container size loading
   butterflyWidth = container.clientWidth;
   butterflyWidth = 600;
   butterflyHeight = container.clientHeight;
-  butterflyHeight = 400;
+  butterflyHeight = 800;
 
   butterflySvg = d3.select("#butterfly-chart-svg")
   .append("svg")
@@ -31,13 +45,13 @@ function initButterflyChart() {
   .range([butterflyWidth / 2 + centerOffset, butterflyWidth - butterflyMargin.right]);
 
   const y = d3.scaleBand()
-  .domain(butterFlyData.map(d => d["Entity"]))
+  .domain(renderedData.map(d => d["Entity"]))
   .range([butterflyMargin.top, butterflyHeight - butterflyMargin.bottom])
   .padding(0.2);
 
   // 3. Draw Left Bars
   butterflySvg.selectAll(".bar-left")
-  .data(butterFlyData)
+  .data(renderedData)
   .enter().append("rect")
   .attr("class", "bar-left")
   .attr("x", d => xLeft(d["Insurance"]))
@@ -48,18 +62,18 @@ function initButterflyChart() {
 
   // 4. Draw Right Bars
   butterflySvg.selectAll(".bar-right")
-  .data(butterFlyData)
+  .data(renderedData)
   .enter().append("rect")
   .attr("class", "bar-right")
   .attr("x", xRight(0))
   .attr("y", d => y(d["Entity"]))
-  .attr("width", d => xRight(100 - d["Mortality"]) - xRight(0))
+  .attr("width", d => xRight(d["Mortality"]) - xRight(0))
   .attr("height", y.bandwidth())
   .attr("fill", "crimson");
 
   // 5. Draw Central Labels
   butterflySvg.selectAll(".label")
-  .data(butterFlyData)
+  .data(renderedData)
   .enter().append("text")
   .attr("x", butterflyWidth / 2)
   .attr("y", d => y(d["Entity"]) + y.bandwidth() / 2)
@@ -70,7 +84,7 @@ function initButterflyChart() {
 
 
   //axis above the chart
-  const axisYPosition = y(butterFlyData[0]["Entity"]) - 5;
+  const axisYPosition = y(renderedData[0]["Entity"]) - 5;
   const xAxisLeft = d3.axisTop(xLeft)
   .ticks(5);
 
@@ -96,6 +110,19 @@ function initButterflyChart() {
   .attr("y", 20)
   .attr("text-anchor", "middle")
   .text("Percentage of population covered by health insurance vs child mortality rate");
+
+
+
+  //slider
+  var slider = document.getElementById("yearSlider");
+
+// Update the current slider value (each time you drag the slider handle)
+  slider.oninput = function() {
+    //ToDo: implement correct data updates
+    console.log(this.value);
+    renderedData = butterFlyData.filter(d => d.Year == this.value)
+    console.log(renderedData)
+  }
 }
 
 function setupDefaultData(year) {
